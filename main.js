038f1cbfbd28e50e94c7f25d74f809a5a488b6e6
Aditya21326 <80681858+Aditya21326@users.noreply.github.com>
2021-05-02 23:47:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+rect()
+ctx.beginPath();
+ctx.strokeStyle - "red";
+ctx.lineWidth - 1;
+ctx.rect(150, 141, 430, 200)
+ctx.stroke();
+
+arc(b)
+ctx.behinPath();
+ctx.strokeStyle - "blue";
+ctx.lineWidth - 5;
+ctx.arc(250 ,210, 40, 0, 2* Math.PI);
+ctx.stroke();
+
+arc(a)
+ctx.behinPath();
+ctx.strokeStyle - "black";
+ctx.lineWidth - 5;
+ctx.arc(250 ,210, 40, 0, 2* Math.PI);
+ctx.stroke();
+
+arc (c)
+ctx.behinPath();
+ctx.strokeStyle - "red";
+ctx.lineWidth - 5;
+ctx.arc(250 ,210, 40, 0, 2* Math.PI);
+ctx.stroke();
+
+arc(d)
+ctx.behinPath();
+ctx.strokeStyle - "yellow";
+ctx.lineWidth - 5;
+ctx.arc(250 ,210, 40, 0, 2* Math.PI);
+ctx.stroke();
+arce
+ctx.behinPath();
+ctx.strokeStyle - "green";
+ctx.lineWidth - 5;
+ctx.arc(250 ,210, 40, 0, 2* Math.PI);
+ctx.stroke();
